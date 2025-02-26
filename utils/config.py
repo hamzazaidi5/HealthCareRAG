26d48a4648899058e44291ba2531eee2839c7799
Syed Haider Ali Zaidi <haiderali.zaidi@conovoinc.com>
2025-02-26 11:45:20 +0500

    changes completed

--- utils/config.py
+++ utils/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 class Config:
     GROQ_API_KEY = os.getenv("GROQ_API_KEY")
     # EMBEDDING_MODEL = "sentence-transformers/all-mpnet-base-v2"
-    LLM_MODEL = "mixtral-8x7b-32768"
+    LLM_MODEL = "llama-3.3-70b-versatile"
     CSV_PATH = "data/oncology_survival_summaries.csv"
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     EMBEDDING_MODEL = "text-embedding-3-small"
